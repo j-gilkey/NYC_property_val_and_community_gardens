23c5acf0f0b7e7b13af75d8b1f6a5702e7db9961
“jackson_gilkey” <jackson.gilkey@gmail.com>
2020-03-16 11:22:43 -0400

    refactored and commented the garden_locator script, removed an unused API access script

--- garden_locator.py
+++ garden_locator.py
@@ -4,32 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import NearestNeighbors
 
-# lots_df = pd.read_csv('data/man_df.csv')
-# garden_df = pd.read_csv('data/df_location_2.csv')
-#
-# columns_to_keep = ['borough', 'block', 'lot', 'cd', 'zipcode', 'address', 'zonedist1', 'splitzone', 'bldgclass', 'landuse', 'ownertype', 'ownername', 'lotarea', 'bldgarea', 'latitude', 'longitude']
-#
-# lots_df = lots_df.loc[:, columns_to_keep].dropna()
-# garden_df = garden_df.loc[:, columns_to_keep].dropna()
-#
-# print(lots_df.head)
-# print(garden_df.shape)
-
 def find_nearby_gardens(lots_df):
+    #takes in a dataframe that contains 'lat' and 'lng' columns representing lattitude and longitude coordinates
+    #for each of those points this function adds the distance in feet to the nearest manhattan comunity garden
 
     garden_df = pd.read_csv('data/df_location_2.csv')
+    #read in the list of gardens
 
     gar_loc_df = garden_df.loc[:, ['latitude', 'longitude']]
     lots_loc_df = lots_df.loc[:, ['lat', 'lng']]
+    #trim both dataframes to only contain latitude and longitude columns, allowing it to be fed into KNN
 
     neigh = NearestNeighbors(1)
     neigh.fit(gar_loc_df)
-
-    #print(lots_loc_df.iloc[:2])
-
-    #print(neigh.kneighbors(lots_loc_df, return_distance=True))
+    #instantiate NN and fit it to the garden locations
 
     lots_df['distance_to_garden'] = [(i[0]*364286) for i in neigh.kneighbors(lots_loc_df, return_distance=True)[0]]
+    # calculate and add the distance_to_garden column for each row in the input df
 
     return lots_df
 
